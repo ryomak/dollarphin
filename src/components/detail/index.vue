2ca83414a13232f181c82550127ab80edf75a35a
ryomak <ryooomaaa0413@outlook.com>
2018-10-12 15:34:57 +0900

    init

--- src/components/detail/index.vue
+++ src/components/detail/index.vue
@@ -1,9 +1,16 @@
 <template>
 	<div class="detail" >
 		<div class="detail-box">
+			<h2><span>Agenda</span></h2>
+			<div class="letter">
+				<p>17:00 ~  open <br>more suprize<br>20:00 ~  close</p>
+			</div>
+		</div>
+		<div class="detail-box">
 			<h2><span>Schedule</span></h2>
 			<div class="date">
-				<h4>2018年11月30日　17:00~20:00</h4>
+				<h5>2018年11月30日</h5>
+				　<h5>17:00~20:00</h5>
 			</div>
 		</div>
 		<div class="detail-box">
@@ -17,7 +24,7 @@
 					<li>
 						<div class="map">
 							<!-- GOOGLE MAP -->
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.203164150161!2d135.5043997148711!3d34.674821580441034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e73d892eb2f7%3A0x8e46540b5e3b4312!2sG2!5e0!3m2!1sja!2sjp!4v1539260830171" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.203164150161!2d135.5043997148711!3d34.674821580441034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e73d892eb2f7%3A0x8e46540b5e3b4312!2sG2!5e0!3m2!1sja!2sjp!4v1539260830171" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 							<!-- // GOOGLE MAP -->
 						</div>
 					</li>
@@ -36,11 +43,6 @@
 	margin-bottom: 5px;
 }
 
-.col2 li{
-	display: inline-block;
-	width: 100%;
-	margin: 20px 0;
-}
 .detail{
 	text-align:center;
 	display:flex;
@@ -54,20 +56,33 @@
 		text-align:center;
 		padding-top:5rem;
 	}
-	.map{
-		position: relative;
-		padding-bottom: 75%;
-		height: 0;
-		overflow: hidden;
-		z-index: 0;
-		iframe{
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 80% !important;
-			height: 80% !important;
-			border: 0;
-		}	
+	.letter{
+    	background-image:linear-gradient(90deg,rgba(204,204,204,0) 0%,rgba(204,204,204,0) 49%,rgba(255,255,255,100) 50%,rgba(255,255,255,100) 100%),linear-gradient(180deg,rgba(204,204,204,0) 0%,rgba(204,204,204,0) 98.5%,rgba(100,100,100,100) 100%);  
+      	background-repeat:repeat-x,repeat-y;
+      	background-size:4px 100%,100% 2.4em;
+      	line-height:2.4;
+        p {
+          margin-bottom:2.4em;
+        }
+	}
+	.cols{
+		li{
+			display: inline-block;
+			.map{
+				position: relative;
+				width:70%;
+				height:0;
+				padding-bottom: 56.25%;    /* padding-topでもOK */
+				z-index: 0;
+				iframe{
+					top: 0;
+					left: 0;
+					width: 100% !important;
+					height: 100% !important;
+				}	
+			}
+			
+		}
 	}
 }
 
--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -24,7 +24,7 @@
 </template>
 <script>
 	export default{
-		name:"HeaderCompont"
+		name:"HeaderCompont",
 	}
 	
 </script>
