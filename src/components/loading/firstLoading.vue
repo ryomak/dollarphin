8e8657506b61d96536ce5685405c3b7e9ece40a0
ryomak <ryooomaaa0413@outlook.com>
2018-10-20 00:43:12 +0900

    deploy

--- src/components/loading/firstLoading.vue
+++ src/components/loading/firstLoading.vue
@@ -4494,28 +4494,18 @@
 }
 @keyframes heart_line {
 	0% {transform: scale(0.8,1); }
-	10% { transform: scale(1,0.8); }
-	20% { transform: scale(0.8,1); }
-	30% { transform: scale(1,0.8); }
+	20% { transform: scale(1,0.8); }
 	40% { transform: scale(0.8,1); }
-	50% { transform: scale(1,0.8); }
-	60% { transform: scale(0.8,1); }
-	70% { transform: scale(1,0.8); }
+	60% { transform: scale(1,0.8); }
 	80% { transform: scale(0.8,1); }
-	90% { transform: scale(1,0.8); }
 	100% { transform: scale(1,1); }
 }
 @-webkit-keyframes heart_line {
 	0% {transform: scale(0.8,1); }
-	10% { transform: scale(1,0.8); }
-	20% { transform: scale(0.8,1); }
-	30% { transform: scale(1,0.8); }
+	20% { transform: scale(1,0.8); }
 	40% { transform: scale(0.8,1); }
-	50% { transform: scale(1,0.8); }
-	60% { transform: scale(0.8,1); }
-	70% { transform: scale(1,0.8); }
+	60% { transform: scale(1,0.8); }
 	80% { transform: scale(0.8,1); }
-	90% { transform: scale(1,0.8); }
 	100% { transform: scale(1,1); }
 }
 </style>
